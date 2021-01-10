0eec753714412e70d5ce3edcfc7a9c4ebfbc758d
FuzzyMelon94 <tom--brown1994@hotmail.com>
2021-01-10 23:26:10 +1100

    Update boot.lua

--- boot.lua
+++ boot.lua
@@ -1,4 +1,8 @@
--- v0.1.7
+-- v0.1.8
+-- Load required files
+os.loadAPI("monitor")
+os.loadAPI("logo")
+
 -- Monitor settings
 local mon = peripheral.wrap("top")
 local monWidth, monHeight = mon.getSize()
@@ -13,95 +17,95 @@ local cursorBlinkRate = 0.5
 local title = "=== Mini0n OS [v0.1.7] ==="
 local state = "Booting..."
 
--- Prepares the monitor for use
-function setupMonitor()
-    mon.clear()
-    mon.setTextColor(colors.yellow)
-    mon.setBackgroundColor(colors.black)
-    mon.setTextScale(0.5)
-    mon.clear()
-end
+-- -- Prepares the monitor for use
+-- function setupMonitor()
+--     mon.clear()
+--     mon.setTextColor(colors.yellow)
+--     mon.setBackgroundColor(colors.black)
+--     mon.setTextScale(0.5)
+--     mon.clear()
+-- end
 
 -- Write a line at a position, with character delay and clearing if required
-function writeLine(s, col, row, clear)
-    local col = col or 1
-    local row = row or 1
-    local clear = clear or false
+-- function writeLine(s, col, row, clear)
+--     local col = col or 1
+--     local row = row or 1
+--     local clear = clear or false
 
-    for i = 0, #s do
-        mon.setCursorPos(col + i, row)
-        mon.write(s:sub(i, i))
-        sleep(writeSpeed)
-    end
+--     for i = 0, #s do
+--         mon.setCursorPos(col + i, row)
+--         mon.write(s:sub(i, i))
+--         sleep(writeSpeed)
+--     end
 
-    if clear then
-        for i = col + #s + 1, monWidth do
-            mon.setCursorPos(i, row)
-            mon.write(" ")
-        end
-    end
-end
+--     if clear then
+--         for i = col + #s + 1, monWidth do
+--             mon.setCursorPos(i, row)
+--             mon.write(" ")
+--         end
+--     end
+-- end
 
--- Moves a line up or down the monitor
-function moveLines(s, col, rowStart, distance)
-    local rowStart = rowStart or 1
-    local distance = 5
-    local isUp = distance < 0
+-- -- Moves a line up or down the monitor
+-- function moveLines(s, col, rowStart, distance)
+--     local rowStart = rowStart or 1
+--     local distance = 5
+--     local isUp = distance < 0
 
-    writeLine(s, col, rowStart, true)
-    for i = 1, distance do
-        writeLine(s, col, rowStart + i, true)
-        writeLine(" ", col, rowStart + i - 1, true)
-    end
-    writeLine(" ", col, rowStart + distance - 1, true)
-end
+--     writeLine(s, col, rowStart, true)
+--     for i = 1, distance do
+--         writeLine(s, col, rowStart + i, true)
+--         writeLine(" ", col, rowStart + i - 1, true)
+--     end
+--     writeLine(" ", col, rowStart + distance - 1, true)
+-- end
 
--- Display a spinner at a position for a duration
-function displaySpinner(col, row, duration)
-    local spinA = "-"
-    local spinB = "\\"
-    local spinC = "|"
-    local spinD = "/"
+-- -- Display a spinner at a position for a duration
+-- function displaySpinner(col, row, duration)
+--     local spinA = "-"
+--     local spinB = "\\"
+--     local spinC = "|"
+--     local spinD = "/"
 
-    for i = 1, duration do
-        step = i % 4
-        mon.setCursorPos(col, row)
+--     for i = 1, duration do
+--         step = i % 4
+--         mon.setCursorPos(col, row)
 
-        if step == 0 then
-            mon.write(spinA)
-        elseif step == 1 then
-            mon.write(spinB)
-        elseif step == 2 then
-            mon.write(spinC)
-        else
-            mon.write(spinD)
-        end
+--         if step == 0 then
+--             mon.write(spinA)
+--         elseif step == 1 then
+--             mon.write(spinB)
+--         elseif step == 2 then
+--             mon.write(spinC)
+--         else
+--             mon.write(spinD)
+--         end
 
-        sleep(spinnerSpeed)
-    end
+--         sleep(spinnerSpeed)
+--     end
 
-    mon.setCursorPos(col, row)
-    mon.write(spinA)
-end
+--     mon.setCursorPos(col, row)
+--     mon.write(spinA)
+-- end
 
--- ! Display blinking caret - interrupt to be added
-function displayBlinking(c, col, row, duration)
-    local c = c or "_"
-    local col = col or 1
-    local row = row or 1
-    local duration = duration or 60
-    local showing = false
+-- -- Display blinking caret
+-- function displayBlinking(c, col, row, duration)
+--     local c = c or "_"
+--     local col = col or 1
+--     local row = row or 1
+--     local duration = duration or 60
+--     local showing = false
 
-    for i = 0, duration do
-        mon.setCursorPos(col, row)
-        if i % 2 == 0 then
-            mon.write(c)
-        else
-            mon.write(" ")
-        end
-        sleep(cursorBlinkRate)
-    end
-end
+--     for i = 0, duration do
+--         mon.setCursorPos(col, row)
+--         if i % 2 == 0 then
+--             mon.write(c)
+--         else
+--             mon.write(" ")
+--         end
+--         sleep(cursorBlinkRate)
+--     end
+-- end
 
 -- Main program
 setupMonitor()
@@ -121,3 +125,4 @@ writeLine(title, midX - (#title / 2), 1, true)
 writeLine(">", 1, monHeight, true)
 sleep(0.5)
 displayBlinking("_", 2, monHeight, 30)
+
